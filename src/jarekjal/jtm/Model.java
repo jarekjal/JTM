2d2cf610c3cb2155e3541eb319f3258c1a24a250
jarekjal <jarekjal@wp.pl>
2018-02-25 12:55:20 +0100

    pierwsze proby z odgrywaniem plikow

--- src/jarekjal/jtm/Model.java
+++ src/jarekjal/jtm/Model.java
@@ -2,6 +2,8 @@ package jarekjal.jtm;
 
 import jarekjal.utils.FileUtils;
 import jarekjal.utils.ListUtils;
+import javafx.scene.media.AudioClip;
+
 import java.io.File;
 import java.util.List;
 import java.util.Observable;
@@ -12,21 +14,23 @@ import java.util.function.Predicate;
  */
 public class Model extends Observable {
 
+
+
     private enum State {S0, S1, S2, S3, S4, S5, S6}
     private State state = State.S0;
     private static File dir = null;
     private static List<File> randomFiles = null;
     private static final int DEFAULT_COUNT = 7;
     private static int randomFilesCount = DEFAULT_COUNT;
+    private static int ptr = 0;
     private static Predicate<File> isMP3 = (File f) -> f.toString().toLowerCase().endsWith(".mp3");
-
-
+    AudioClip clip = null;
 
     public void setDir(File dir) {
         Model.dir = dir;
 
         List<File> fileList = FileUtils.flatFileTree(this.dir, isMP3);
-        System.out.println("odfiltrowano: " + fileList.size());
+        System.out.println("Odfiltrowano: " + fileList.size());
         if (fileList.isEmpty()) {
             this.state = State.S0;
             this.setChanged();
@@ -45,6 +49,28 @@ public class Model extends Observable {
     }
 
 
+    public void spacePressed() {
+        switch (state){
+            case S0: // wybieranie katalogu
+                break;
+            case S1: // przed rozpoczeciem gry
+                clip = new AudioClip(randomFiles.get(ptr).toURI().toString());
+                clip.play();
+                ptr++; // ustaw na nastepny z listy (bez sprawdzania czy istnieje)
+                state = State.S2;
+                break;
+            case S2:
+                if (ptr > randomFiles.size()-1) {
+                    clip.stop();
+                    ptr = 0;
+                    state = State.S1;
+                } else {
+                    clip.stop();
+                    state = State.S1;
+                }
+                break;
+        }
 
+    }
 
 }
