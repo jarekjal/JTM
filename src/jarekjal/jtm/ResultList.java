1721132cc6b488a2b3ddb12b0d6e01f5b5e96296
jarekjal <jarekjal@wp.pl>
2018-03-03 19:13:19 +0100

    pierwsza dzialajacawersja z wynikami

--- src/jarekjal/jtm/ResultList.java
+++ src/jarekjal/jtm/ResultList.java
@@ -1,30 +1,52 @@
 package jarekjal.jtm;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ResultList {
 
     private ResultList(){}
     private Map<File, Duration> map = null;
+    private List<Utwor> lista = null;
+    private ObservableList<Utwor> data = null;
+    @FXML TableColumn colName;
+    @FXML TableColumn colTime;
 
     public static ResultList getInstance() {
         return instance;
     }
 
     private static ResultList instance = new ResultList();
-    @FXML private static TableView lista;
+    @FXML private  TableView<Utwor> tabela;
 
     public void setMap(Map<File,Duration> map) {
         this.map = map;
+        lista = new ArrayList<>();
+        for (File f : map.keySet()){
+            Utwor u = new Utwor(f.getName().toString(), Stoper.formatDuration(map.get(f)));
+            lista.add(u);
+        }
+        data = FXCollections.observableList(lista);
+
+
     }
 
     public void createResultListWindow() {
@@ -44,7 +66,48 @@ public class ResultList {
         }
         properties.setTitle("Results");
 
+        colName.setCellValueFactory(
+                new PropertyValueFactory<Utwor, String>("name"));
+
+
+
+        colTime.setCellValueFactory(
+                new PropertyValueFactory<Utwor, String>("time"));
+
+        tabela.setItems(data);
+        //tabela.getColumns().addAll(colName, colTime);
+
+
         properties.show();
 
     }
+
+    public static class Utwor{
+        private final SimpleStringProperty name;
+        private final SimpleStringProperty time;
+
+        private Utwor(String n, String t){
+            name = new SimpleStringProperty(n);
+            time = new SimpleStringProperty(t);
+        }
+
+        public String getName() {
+            return name.get();
+        }
+
+        public String getTime() {
+            return time.get();
+        }
+
+        public void setTime(String t){
+            time.set(t);
+        }
+
+        public void setName(String n){
+            name.set(n);
+        }
+
+
+    }
+
 }
